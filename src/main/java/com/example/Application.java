1fbb36dc75bbbf5f4927626a5608301a3b76d713
Ihor Didyk <ihordidyk95@gmail.com>
2024-03-04 08:45:17 +0200

    add logic for exec python code from separate files

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,7 +1,43 @@
 package com.example;
 
-public class Application {
-    public static void main(String[] args) {
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotAccess;
+import org.graalvm.polyglot.Value;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Application {
+    private static final String PYTHON_LANG_ID = "python";
+
+    public static void main(String[] args) {
+        String pythonScriptPath = ""; // Update this with the full path to your Python script
+
+        execPythonFileByPath(pythonScriptPath);
+    }
+
+    private static void execPythonFileByPath(String pythonScriptPath) {
+        try (Context context = Context.newBuilder()
+                .option("engine.WarnInterpreterOnly", "false")
+                .allowAllAccess(true)
+                .build()) {
+            Path path = Paths.get(pythonScriptPath);
+
+            if (Files.exists(path)) {
+                Value value = context.eval(PYTHON_LANG_ID, "exec(open('" + pythonScriptPath + "').read())");
+                if (value.isString()) {
+                    String valueString = value.asString();
+                    System.out.println("valueString = " + valueString);
+                } else {
+                    System.out.println("value = " + value);
+                }
+
+            } else {
+                System.out.println("File not found: " + pythonScriptPath);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
