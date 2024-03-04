7f8450b1e628797de006391fb9532fd7642cb1a3
Ihor Didyk <ihordidyk95@gmail.com>
2024-03-04 08:45:46 +0200

    add more logic to python script

--- python/test.py
+++ python/test.py
@@ -1,3 +1,26 @@
+import subprocess
+
+def install_requirements(requirements_file):
+    with open(requirements_file, 'r') as f:
+        packages = f.readlines()
+
+    for package in packages:
+        package = package.strip()
+        subprocess.call(['pip', 'install', package])
+
+    print("Installed packages from requirements file")
+
+
+def get_post():
+    url = 'https://jsonplaceholder.typicode.com/posts/10'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Response JSON:")
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}")
+
 
 if __name__ == "__main__":
     print("Message from separate py file")
