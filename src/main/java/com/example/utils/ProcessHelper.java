3945f934f3185751674270c4fe4ee62fa8254ecc
Ihor Didyk <ihordidyk95@gmail.com>
2024-03-10 09:53:05 +0200

    add util for manage processes

---
+++ src/main/java/com/example/utils/ProcessHelper.java
@@ -0,0 +1,74 @@
+package com.example.utils;
+
+import lombok.experimental.UtilityClass;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+@UtilityClass
+public class ProcessHelper {
+
+    private static final List<String> ROOT_PWD = List.of("sh", "-c", "pwd");
+
+    public static void executePythonProcess(List<String> commands) {
+        final ProcessBuilder pb = new ProcessBuilder(commands);
+        pb.inheritIO();
+        try {
+            Process process = pb.start();
+            process.waitFor();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static String getProjectRoot() {
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command(ROOT_PWD);
+        pb.directory(new File("."));
+        String output = "";
+        try {
+            Process process = pb.start();
+            // Read the output of the command
+            output = readProcessOutput(process);
+
+            // Wait for the process to finish
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                System.err.println("Failed to retrieve project root directory.");
+            }
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println(e.getMessage());
+        }
+
+        // Trim any leading/trailing whitespace and return the result
+        return output.trim();
+    }
+
+    private static String readProcessOutput(Process process) throws IOException {
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(System.lineSeparator());
+            }
+        }
+        return output.toString();
+    }
+
+}
